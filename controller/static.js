aab6f025e04fd7956492e12fb77e86541a9706a8
Ding Jian <djqq1987@gmail.com>
2012-06-30 09:14:15 +0800

    删除

--- controller/static.js
+++
@@ -1,9 +0,0 @@
-exports.index = function(req,res,next){
-	res.render("index");
-}
-exports.version = function(req,res,next){
-	res.render("version");
-}
-exports.admin = function(req,res,next){
-	res.send("<div>121221</div>");
-}
